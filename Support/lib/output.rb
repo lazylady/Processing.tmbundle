b875f40143564d5f7ae4db237d699e0f0f0c28ec
Ted Broberg <ted@lazylady.se>
2013-01-08 22:46:54 +0100

    New Build System and Output Log
    Added new build system which outputs to a log, the TM HTML Output
    window. Support for error reporting and clicking to go to file/line.
    Also coloring lines when keywords are used: #warn, #fatal, #error,
    #debug, e.g println("I'll give you something to DEBUG about #debug");

---
+++ Support/lib/output.rb
@@ -0,0 +1,62 @@
+# 
+#  output.rb
+#  Processing.tmbundle
+#  
+#  Formats Processing log to be used for Textmate HTML output
+#  Created by Ted Broberg on 2013-01-08.
+# 
+
+class Output
+  
+  def initialize
+    @line_count = 0;  
+    @generic_error = /(^[^:]+).([0-9]+).([0-9|:]*).(.*)/
+    
+    # Custom user log types for some coloring of the log
+    @log_error = /(#error)/
+    @log_warn = /(#warning|#warn)/
+    @log_fatal = /(#fatal)/
+    @log_debug = /(#debug)/
+  end
+  
+  def print_line(line)
+    
+    output = parse_line(line)
+    output
+  end
+  
+  def parse_line(line)
+    
+    @line_count = @line_count + 1
+    project_path = ENV['TM_PROJECT_DIRECTORY']
+    out = ""
+    
+    match = @generic_error.match(line)
+    if match
+      out << "<br/><div id='error'>"
+      error = match[4]
+      error = error.first.capitalize
+      out << error + ' <a title="Click to open ' + match[1] +
+      '" href="txmt://open?url=file://' + project_path + "/" + match[1] + '&line=' + match[2] + '" >' + match[1] + '</a><br/>'
+    else
+      log_type = ""
+      match = ""
+      if match = @log_error.match(line)
+        log_type = match[1]
+      elsif match = @log_warn.match(line)
+        log_type = match[1]
+      elsif match = @log_fatal.match(line)
+        log_type = match[1]
+      elsif match = @log_debug.match(line)
+        log_type = match[1]
+      else
+        log_type = "normal"
+      end
+      line.gsub!(log_type, "")
+      log_type.gsub!("#", "")
+      out << "<div id='outPutRow#{@line_count % 2}'>" << "<div id='log_#{log_type}'>" << line << "</div>"
+    end
+    out << "</div>"
+    out
+  end
+end
---
+++ Support/lib/run.rb
@@ -0,0 +1,88 @@
+#!/usr/bin/env ruby -wKU
+
+# 
+#  Run
+#  Processing.tmbundle
+#  Build and run the sketch and create the output
+#  
+#  Created by Ted Broberg on 2013-01-07.
+
+require ENV['TM_SUPPORT_PATH'] + '/lib/textmate'
+require ENV['TM_SUPPORT_PATH'] + '/lib/tm/process'
+require ENV['TM_BUNDLE_SUPPORT'] + '/lib/output'
+require ENV['TM_SUPPORT_PATH'] + '/lib/web_preview'
+require 'fileutils'
+
+class Run
+
+  @project_path = nil
+  @bundle_support = nil
+  @folder_name = nil
+  
+  def initialize
+    
+    @bundle_support = ENV['TM_BUNDLE_SUPPORT']
+    file_path = ENV['TM_FILEPATH']
+    
+    if file_path
+      @project_path = ENV['TM_PROJECT_DIRECTORY']
+      @folder_name = File.basename @project_path
+    else
+      @project_path = "/tmp/textmate-processing/TmpClass"
+      FileUtils.mkdir_p @project_path unless File.directory? @project_path
+      @folder_name = File.basename @project_path
+      f = File.open("#{@project_path}/#{@folder_name}.pde", 'w')
+      f.write STDIN.read
+      f.close
+    end
+  end
+
+  def buildSketch(extra_arg = "")
+    
+    start_time = Time.now
+    cmd = "processing-java --run --force --sketch=#{@project_path} --output=#{@project_path}/build" + extra_arg
+    
+    TextMate.require_cmd("processing-java")
+    init_html(cmd)
+    STDOUT.sync = true
+    
+    output = Output.new
+    
+    TextMate::Process.run(cmd) do |str|
+      STDOUT << output.print_line(str) + force_refresh
+    end
+    
+    end_time = Time.now
+    puts "Sketch did end at #{end_time} and did run for a total of #{end_time - start_time} seconds."
+    puts "</div>#{force_refresh}"
+    # Only works every other time
+    # print "<script type='text/javascript' charset='utf-8'>
+    # window.close();
+    # </script>"
+  end
+  
+  # Force scroll update to html output as this for some reason does not work (autoScroll prop anywhere?)
+  def force_refresh
+    refresh_html = ""
+    refresh_html << "<script type='text/javascript' charset='utf-8'>
+      var element = document.getElementById('outputContent');
+      document.body.scrollTop = element.offsetHeight;
+    </script>"
+    refresh_html
+  end
+  
+  # Print initial html header.
+  def init_html(cmd)
+    
+    puts "<link rel='stylesheet' href='file://#{e_url(@bundle_support)}/css/output.css' type='text/css' charset='utf-8' media='screen'>"
+    puts "<script src='file://#{e_url(@bundle_support)}/js/output.js' type='text/javascript' charset='utf-8'></script>"
+    print "<div id='top'>
+    <div id='title'>Running sketch: #{@folder_name} &nbsp;</div><div id='subtitle'>#{@project_path}</div></div>"
+    puts "<div id='outputContent'>"
+      
+  end
+end
+
+# if __FILE__ == $0
+#     
+# end
